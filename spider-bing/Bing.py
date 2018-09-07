79f03993182756b637159d7cddfa3aae159fa979
devnotes <taonyzhang@gmail.com>
2018-09-07 11:22:35 +0800

    bing spider

--- spider-bing/Bing.py
+++ spider-bing/Bing.py
@@ -1,5 +1,7 @@
 # -*-coding:utf-8-*-
-
+"""
+从bing网站上，定时下载壁纸图片
+"""
 import request
 import urllib
 from bs4 import BeautifulSoup
@@ -8,22 +10,28 @@ import request
 import json
 import random
 
-img_root="https://cn.bing.com/"
+img_root = "https://cn.bing.com/"
 
-def getHtml(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-    req = urllib.request.Request(url=url, headers=headers)
-    html = urllib.request.urlopen(req).read()
-    print(html)
-    js=json.loads(html)
-    img_url=js["images"][0]["url"]
-    print(img_url)
-    saveImg(img_root+img_url)
 
-def saveImg(imgUrl):
-    print(imgUrl)
-    path = "C:/Users/Administrator/Pictures/" + str(random.randint(1, 99999)) + ".jpg"
-    urllib.request.urlretrieve(imgUrl, path)
+class BingSpider:
 
-for i in range(7):
-    getHtml("https://cn.bing.com/HPImageArchive.aspx?format=js&idx="+str(i)+"&n=1")
+    def getHtml(self, url):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+        req = urllib.request.Request(url=url, headers=headers)
+        html = urllib.request.urlopen(req).read()
+        print(html)
+        js = json.loads(html)
+        img_url = js["images"][0]["url"]
+        print(img_url)
+        self.saveImg(img_root + img_url)
+
+    def saveImg(imgUrl):
+        path = "C:/Users/Administrator/Pictures/" + str(random.randint(1, 99999)) + ".jpg"
+        urllib.request.urlretrieve(imgUrl, path)
+
+    def run(self):
+        for i in range(7):
+            self.getHtml("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=" + str(i) + "&n=1")
+
+spider=BingSpider()
+spider.run();
