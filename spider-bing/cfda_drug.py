b5980794635ae300615aafb208b8dd8cb1ecfa88
devnotes <taonyzhang@gmail.com>
2018-09-14 23:20:15 +0800

    bing spider

--- spider-bing/cfda_drug.py
+++
@@ -1,7 +0,0 @@
-# -*-coding:utf-8-*-
-# cfda
-
-"""
-http://app2.sfda.gov.cn/datasearchp/gzcxSearch.do?formRender=cx
-http://app2.sfda.gov.cn/datasearchp/all.do?tableName=TABLE25&formRender=cx&searchcx=&name=%E8%91%A1%E8%90%84%E7%B3%96%E6%B3%A8%E5%B0%84%E6%B6%B2
-"""
